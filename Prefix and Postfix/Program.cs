b233bb3377fb232c6c8882c89475a256664baeb7
boriskisiov <boriskisiov@gmail.com>
2023-10-19 10:53:31 +0300

    Add project files.

---
+++ Prefix and Postfix/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prefix_and_Postfix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(--a);
+            Console.WriteLine(a);
+        }
+    }
+}
